1ca0fe70381d64d330c9c9518a509d3d7e549ef1
MRadzynski <68923217+MRadzynski@users.noreply.github.com>
2021-03-24 11:57:10 +0100

    CRA initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import './index.css';
+
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
