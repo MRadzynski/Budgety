ff4989c7116046844d13d118ebab23598605c19d
MRadzynski <68923217+MRadzynski@users.noreply.github.com>
2021-04-02 15:20:59 +0200

    Adding firebase and creating signUp and signIn with email and password mechanism

--- src/components/sign-in-up/sign-in-up.component.jsx
+++ src/components/sign-in-up/sign-in-up.component.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { auth } from '../../firebase/firebase.utils';
+
 import {
   SignInUpContainer,
   Form,
@@ -8,35 +10,104 @@ import {
   FormSubtext,
   LogoContainer,
   FormInput,
+  ErrorMessageContainer,
 } from './sign-in-up.styles';
 
 import Logo from '../../assets/logo.png';
 
-const SignInUp = ({ formType }) => (
-  <SignInUpContainer>
-    <FormTitle>Budgety</FormTitle>
-    <LogoContainer>
-      <img src={Logo} alt="Budgety logo" />
-    </LogoContainer>
-    <Form>
-      <FormInput type="email" placeholder="Email" required />
-      <FormInput type="password" placeholder="Password" required />
-      {formType === 'sign-up' ? (
-        <FormInput type="password" placeholder="Confirm password" required />
-      ) : null}
-      <CustomButton
-        bgColor={'var(--white-shade)'}
-        textColor={'var(--black-shade)'}
-      >
-        {formType === 'sign-up' ? 'Sign Up' : 'Sign In'}
-      </CustomButton>
-    </Form>
-    <FormSubtext>
-      {formType === 'sign-up'
-        ? `Already have an account? Sign In!`
-        : `Don't have an account? Sign Up!`}
-    </FormSubtext>
-  </SignInUpContainer>
-);
+const SignInUp = ({ formType }) => {
+  const [userCredentials, setUserCredentails] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [error, setError] = useState('false');
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserCredentails({ ...userCredentials, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { email, password } = userCredentials;
+    setError('false');
+
+    if (formType === 'sign-up') {
+      if (userCredentials.password !== userCredentials.confirmPassword) {
+        setError('true');
+        return;
+      }
+
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((userData) => {
+          console.log(userData);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      auth
+        .signInWithEmailAndPassword(email, password)
+        .then((userData) => {
+          console.log(userData);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <SignInUpContainer>
+      <FormTitle>Budgety</FormTitle>
+      <LogoContainer>
+        <img src={Logo} alt="Budgety logo" />
+      </LogoContainer>
+      <Form onSubmit={handleSubmit}>
+        <FormInput
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+        {formType === 'sign-up' ? (
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            onChange={handleChange}
+            required
+          />
+        ) : null}
+        <ErrorMessageContainer display={error}>
+          Passwords don't match!
+        </ErrorMessageContainer>
+
+        <CustomButton
+          type="submit"
+          bgColor={'var(--white-shade)'}
+          textColor={'var(--black-shade)'}
+        >
+          {formType === 'sign-up' ? 'Sign Up' : 'Sign In'}
+        </CustomButton>
+      </Form>
+      <FormSubtext>
+        {formType === 'sign-up'
+          ? `Already have an account? Sign In!`
+          : `Don't have an account? Sign Up!`}
+      </FormSubtext>
+    </SignInUpContainer>
+  );
+};
 
 export default SignInUp;
