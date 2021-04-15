8a1a29d62a6afc5b033bb87e3e28fe0fdfd5f8c3
MRadzynski <68923217+MRadzynski@users.noreply.github.com>
2021-04-15 14:43:46 +0200

    Adding redux-persist for maintaining state across app reloads

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import App from './App';
 
 import './index.css';
@@ -12,7 +13,9 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  applyMiddleware(...middlewares)
+);
+
+export const persistor = persistStore(store);
