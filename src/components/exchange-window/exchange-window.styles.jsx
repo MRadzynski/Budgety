bf260e5d87873eda86199a373b2aca947ac8dbab
MRadzynski <radzynski.milosz@gmail.com>
2021-06-20 08:39:05 +0200

    Adjusting exchange page for all devices

--- src/components/exchange-window/exchange-window.styles.jsx
+++ src/components/exchange-window/exchange-window.styles.jsx
@@ -5,8 +5,47 @@ export const ExchangeWindowContainer = styled.div`
   height: 75%;
   display: flex;
   flex-direction: column;
+  padding-top: 0.5rem;
   border-radius: 25px;
   background-color: var(--white-shade);
+
+  @media (min-width: 22.5rem) {
+    margin-top: 2rem;
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    margin-top: 4rem;
+  }
+
+  @media (min-width: 64rem) and (orientation: landscape) {
+    width: 60%;
+    margin-top: 1rem;
+    padding-top: 1rem;
+  }
+
+  @media (min-width: 100rem) and (min-height: 900px) {
+    width: 50%;
+    margin-top: 2rem;
+  }
+
+  @media (min-width: 120rem) {
+    width: 45%;
+    padding-top: 1.5rem;
+  }
+
+  @media (min-width: 160rem) {
+    width: 40%;
+    padding-top: 1.7rem;
+  }
+
+  @media (min-width: 160rem) and (min-height: 90rem) {
+    margin-top: 4rem;
+  }
+
+  @media (min-width: 240rem) {
+    margin-top: 6rem;
+    padding-top: 3rem;
+  }
 `;
 
 export const ExchangeList = styled.ul`
@@ -14,35 +53,181 @@ export const ExchangeList = styled.ul`
   height: 100%;
   display: flex;
   flex-direction: column;
-  list-style: none;
   position: relative;
-  overflow: hidden scroll;
-  border-radius: 25px;
   margin-top: 0.5rem;
+  border-radius: 25px;
+  list-style: none;
+  overflow: hidden scroll;
 `;
 
 export const ExchangeListItem = styled.li`
   display: flex;
   justify-content: space-between;
-  padding: 3rem;
+  padding: 3rem 2rem;
 
   &:nth-child(2n) {
     background-color: #e0e0e0;
   }
+
+  @media (min-width: 22.5rem) {
+    padding: 3rem 2.5rem;
+  }
+
+  @media (min-width: 25rem) {
+    padding: 3rem;
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    padding: 3rem 5rem;
+  }
+
+  @media (min-width: 64rem) and (orientation: landscape) {
+    padding: 3rem 4rem;
+  }
+
+  @media (min-width: 85rem) and (orientation: landscape) {
+    padding: 4rem;
+  }
+
+  @media (min-width: 120rem) {
+    padding: 4rem 5rem;
+  }
+
+  @media (min-width: 160rem) and (min-height: 90rem) {
+    padding: 5rem 7rem;
+  }
+
+  @media (min-width: 240rem) {
+    padding: 7rem 10rem;
+  }
 `;
 
 export const CurrencyRate = styled.h2`
-  letter-spacing: ${({ length }) => (length > 13 ? '0' : '0.1rem')};
   font-size: ${({ length }) => (length > 13 ? '2.2rem' : '2.4rem')};
+  letter-spacing: ${({ length }) => (length > 13 ? '0' : '0.1rem')};
+
+  @media (min-width: 20rem) {
+    font-size: ${({ length }) => (length > 11 ? '1.7rem' : '2rem')};
+  }
+
+  @media (min-width: 22.5rem) {
+    font-size: ${({ length }) => (length > 11 ? '1.9rem' : '2.1rem')};
+  }
+
+  @media (min-width: 25rem) {
+    font-size: ${({ length }) => (length > 11 ? '2.2rem' : '2.4rem')};
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    font-size: 3.2rem;
+  }
+
+  @media (min-width: 64rem) and (orientation: landscape) {
+    font-size: 2.8rem;
+  }
+
+  @media (min-width: 85rem) and (orientation: landscape) {
+    font-size: 3.2rem;
+  }
+
+  @media (min-width: 100rem) and (min-height: 900px) {
+    font-size: 3.6rem;
+  }
+
+  @media (min-width: 120rem) {
+    font-size: 3.8rem;
+  }
+
+  @media (min-width: 160rem) and (min-height: 90rem) {
+    font-size: 4.6rem;
+  }
+
+  @media (min-width: 240rem) {
+    font-size: 7rem;
+  }
 `;
 
 export const CurrencyName = styled.h2`
   font-size: 1.8rem;
   letter-spacing: 0.15rem;
+
+  @media (min-width: 22.5rem) {
+    font-size: 2rem;
+  }
+
+  @media (min-width: 25rem) {
+    font-size: 2.4rem;
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    font-size: 3rem;
+  }
+
+  @media (min-width: 64rem) and (orientation: landscape) {
+    font-size: 2.8rem;
+  }
+
+  @media (min-width: 85rem) and (orientation: landscape) {
+    font-size: 3.2rem;
+  }
+
+  @media (min-width: 100rem) and (min-height: 900px) {
+    font-size: 3.6rem;
+  }
+
+  @media (min-width: 120rem) {
+    font-size: 3.8rem;
+  }
+
+  @media (min-width: 160rem) and (min-height: 90rem) {
+    font-size: 4.6rem;
+  }
+
+  @media (min-width: 240rem) {
+    font-size: 7rem;
+  }
 `;
 
 export const DateInfo = styled.h3`
   color: var(--black-shade);
   font-size: 1.6rem;
   text-align: center;
+
+  @media (min-width: 20rem) {
+    font-size: 1.4rem;
+  }
+
+  @media (min-width: 22.5rem) {
+    padding: 0.3rem 0;
+    font-size: 1.6rem;
+  }
+
+  @media (min-width: 25rem) {
+    padding: 0.4rem 0;
+    font-size: 1.8rem;
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    font-size: 2.6rem;
+  }
+
+  @media (min-width: 64rem) and (orientation: landscape) {
+    font-size: 2.2rem;
+  }
+
+  @media (min-width: 85rem) and (orientation: landscape) {
+    font-size: 2.4rem;
+  }
+
+  @media (min-width: 120rem) {
+    font-size: 2.8rem;
+  }
+
+  @media (min-width: 160rem) and (min-height: 90rem) {
+    font-size: 3.6rem;
+  }
+
+  @media (min-width: 240rem) {
+    font-size: 6rem;
+  }
 `;
--- src/pages/exchange/exchange.styles.jsx
+++ src/pages/exchange/exchange.styles.jsx
@@ -6,21 +6,107 @@ export const ExchangeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 6rem 2rem;
   gap: 3rem;
+  padding: 6rem 2rem;
+
+  @media (min-width: 20rem) {
+    gap: 2rem;
+  }
+
+  @media (min-width: 22.5rem) {
+    gap: 2.5rem;
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    gap: 3.5rem;
+  }
 `;
 
 export const ExchangeTitle = styled.h1`
+  color: var(--white-shade);
   font-size: 3rem;
   text-align: center;
-  color: var(--white-shade);
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  @media (min-width: 20rem) {
+    font-size: 3.2rem;
+  }
+
+  @media (min-width: 22.5rem) {
+    font-size: 3.6rem;
+  }
+
+  @media (min-width: 25rem) {
+    font-size: 4rem;
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    font-size: 6rem;
+  }
+
+  @media (min-width: 64rem) and (orientation: landscape) {
+    font-size: 6rem;
+  }
+
+  @media (min-width: 85rem) and (orientation: landscape) {
+    font-size: 6.2rem;
+  }
+
+  @media (min-width: 120rem) {
+    font-size: 7rem;
+  }
+
+  @media (min-width: 160rem) and (min-height: 90rem) {
+    margin-top: 2rem;
+    font-size: 9rem;
+  }
+
+  @media (min-width: 240rem) {
+    margin-top: 8rem;
+    font-size: 14rem;
+  }
 `;
 
 export const ExchangeSubTitle = styled.h2`
+  color: var(--white-shade);
   font-size: 1.8rem;
   font-weight: normal;
   text-align: center;
-  color: var(--white-shade);
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  @media (min-width: 20rem) {
+    font-size: 1.6rem;
+  }
+
+  @media (min-width: 22.5rem) {
+    font-size: 1.8rem;
+  }
+
+  @media (min-width: 25rem) {
+    font-size: 2.2rem;
+  }
+
+  @media (min-width: 48rem) and (orientation: portrait) {
+    font-size: 3.2rem;
+  }
+
+  @media (min-width: 64rem) and (orientation: landscape) {
+    font-size: 3rem;
+  }
+
+  @media (min-width: 85rem) and (orientation: landscape) {
+    font-size: 3.2rem;
+  }
+
+  @media (min-width: 120rem) {
+    font-size: 3.8rem;
+  }
+
+  @media (min-width: 160rem) and (min-height: 90rem) {
+    font-size: 5rem;
+  }
+
+  @media (min-width: 240rem) {
+    font-size: 7rem;
+  }
 `;
